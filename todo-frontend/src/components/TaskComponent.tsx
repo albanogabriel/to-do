3490ba1ccd3bca59f85f0360647aa75383b31e75
Gabriel Albano <albanogabriel33@gmail.com>
2024-09-30 17:48:34 -0300

    feat: add friendly Interface onDeleteTask

--- todo-frontend/src/components/TaskComponent.tsx
+++ todo-frontend/src/components/TaskComponent.tsx
@@ -1,19 +1,20 @@
-// import { useState } from 'react'
 import trashIcon from '../assets/trashicon.svg'
 import pencilIcon from '../assets/pencil.svg'
 
-// import { taskType } from './TasksArea'
-
 import { Task } from '../types/tasks'
 import styles from './TaskComponent.module.css'
+import { createPortal } from 'react-dom'
+import { EditModal } from './EditModal'
+import { useState } from 'react'
 
 interface TaskComponentProps {
   data: Task
-  // onDeleteTask: (task: taskType) => void
-  // onToggleTask: (task: taskType) => void
+  onDeleteTask: (id: string) => void
+  // onToggleTask: (task: Task) => void
 }
 
-export function TaskComponent({ data }: TaskComponentProps) {
+export function TaskComponent({ data, onDeleteTask }: TaskComponentProps) {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   // const [checked, setChecked] = useState(content.isChecked)
 
   const handleDeleteTask = async () => {
@@ -21,32 +22,43 @@ export function TaskComponent({ data }: TaskComponentProps) {
     await fetch(`http://localhost:3333/tasks/${data.id}`, {
       method: 'DELETE'
     })
+
+    onDeleteTask(data.id)
   }
+
+  // const handleEditTask = async () => {}
 
   const handleChangeChecked = async () => {
     // setChecked(!checked)
   }
 
   return (
-    <div className={styles.divWithTasks}>
-      <div className={styles.divWithTasksContainer}>
-        <div className={styles.divWithTasksContainerCheckBoxP}>
-          <input
-            onClick={handleChangeChecked}
-            type="checkbox"
-            // checked={checked}
-            // onChange={handleChangeInput}
-          />
-          <p>{data.title}</p>
-          <p>{data.description}</p>
-        </div>
-        <div className={styles.buttonContainer}>
-          <button onClick={handleDeleteTask}>
-            <img className={styles.pencilIcon} src={pencilIcon} alt="" />
-          </button>
-          <button onClick={handleDeleteTask}>
-            <img src={trashIcon} alt="" />
-          </button>
+    <div>
+      {modalIsOpen &&
+        createPortal(
+          <EditModal setModalIsOpen={setModalIsOpen} taskId={data.id} />,
+          document.body
+        )}
+      <div className={styles.divWithTasks}>
+        <div className={styles.divWithTasksContainer}>
+          <div className={styles.divWithTasksContainerCheckBoxP}>
+            <input
+              onClick={handleChangeChecked}
+              type="checkbox"
+              // checked={checked}
+              // onChange={handleChangeInput}
+            />
+            <p>{data.title}</p>
+            <p>{data.description}</p>
+          </div>
+          <div className={styles.buttonContainer}>
+            <button onClick={() => setModalIsOpen(true)}>
+              <img className={styles.pencilIcon} src={pencilIcon} alt="" />
+            </button>
+            <button onClick={handleDeleteTask}>
+              <img src={trashIcon} alt="" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
